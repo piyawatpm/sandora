75f49f25eb2257869df3878c43ea9ad873f1cc8b
piyawatpm <piyawat_00101@hotmail.com>
2022-11-30 10:06:36 +1100

    update

--- src/components/Project/Future-project.tsx
+++ src/components/Project/Future-project.tsx
@@ -7,27 +7,45 @@ const FutureProject = () => {
         {" "}
         <FormattedMessage id="project.future.header" />
       </h1>
-      <img src="/images/future_project/wallet.gif" className="sm:w-[500px] sm:h-[604px] mx-auto" alt="" />
-      <div className=" flex sm:flex-row flex-col  sm:space-y-0 space-y-3 sm:space-x-3 px-8 text-justify">
-        <div className=" sm:h-auto h-[230px] sm:w-1/2 flex flex-col justify-around rounded-[36px] bg-gradient-to-b from-[#CC00FF] to-[#320071] p-7 sm:p-12">
-          <div className=" flex flex-col ">
-            <h1 className=" text-[16px] sm:text-[36px] font-black text-transparent textgradient-yellow ">
-              <FormattedMessage id="project.future.wallet.header" />
-            </h1>
-            <div className=" flex items-center justify-center w-full">
-              <img
-                src="/images/future_project/wallet.png"
-                className=" sm:w-auto w-[65px] h-[80px] sm:h-auto"
-                alt=""
-              />
+
+      <div className=" flex flex-col  sm:space-y-5 space-y-3 px-8 text-justify">
+      <img
+            src="/images/future_project/wallet.gif"
+            
+            className=" sm:hidden"
+            alt=""
+          />
+        <div className=" sm:h-auto h-[230px]  flex flex-row justify-around rounded-[36px] bg-gradient-to-b from-[#CC00FF] to-[#320071] p-7 sm:p-12">
+          <img
+            src="/images/future_project/wallet.gif"
+            
+            className="sm:w-[500px] sm:h-[604px] mx-auto sm:block hidden"
+            alt=""
+          />
+          <div className=" flex flex-col justify-center">
+            <div className=" flex flex-row justify-center items-center ">
+              <h1 className=" text-[16px] sm:text-[36px] font-black text-transparent textgradient-yellow ">
+                <FormattedMessage id="project.future.wallet.header" />
+              </h1>
+              <div className=" flex items-center justify-center w-full">
+                <img
+                  src="/images/future_project/wallet.png"
+                  className=" sm:w-auto w-[65px] h-[80px] sm:h-auto"
+                  alt=""
+                />
+              </div>
             </div>
+            <p className=" text-[13px] sm:text-[20px] font-bold">
+              <FormattedMessage id="project.future.wallet.description" />
+            </p>
           </div>
-          <p className=" text-[13px] sm:text-[20px] font-bold">
-            <FormattedMessage id="project.future.wallet.description" />
-          </p>
         </div>
-        <div className="  sm:h-auto h-[230px]  sm:w-1/2 flex flex-col justify-around sm:justify-end rounded-[36px] bg-gradient-to-b from-[#CC00FF] to-[#320071] p-7   sm:p-12">
-          <div className=" flex flex-col ">
+        <img src="/images/future_project/chain-diagram.png" className="" alt="" />
+        <div className="  sm:h-auto h-[230px]   flex flex-row justify-around sm:justify-end rounded-[36px] bg-gradient-to-b from-[#CC00FF] to-[#320071] p-7   sm:p-12">
+       
+          <div className=" flex flex-col justify-center">
+
+          <div className=" flex flex-col  ">
             <h1 className=" text-[16px] sm:text-[36px] font-black text-transparent textgradient-yellow ">
               <FormattedMessage id="project.future.chain.header" />
             </h1>
@@ -42,9 +60,10 @@ const FutureProject = () => {
           <p className=" text-[13px] sm:text-[20px] font-bold sm:mt-[70px]">
             <FormattedMessage id="project.future.chain.description" />
           </p>
+          </div>
+          <img src="/images/future_project/chain-diagram.png" className="sm:block hidden" alt="" />
         </div>
       </div>
-    
     </div>
   );
 };
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,21 @@
 const Video = () => {
   return (
-    <div className="bg-[url('/images/video-bg.png')] bg-cover sm:h-[795px] mix-blend-overlay flex items-center justify-center p-10 sm:p-5">
+    <div className="bg-[url('/images/video-bg.png')] bg-cover sm:h-[795px] mix-blend-overlay relative flex items-center justify-center p-10 sm:p-5">
+      {/* https://storage.googleapis.com/geometric-rex-343406.appspot.com/Video/Sandora%20Wallet.mp4 */}
         <img src="/images/computer.png" className=" mix-blend-normal" alt="" />
+        <video
+        width="802"
+        height="508"
+        className=" mx-auto absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]"
+        controls
+        autoPlay
+        loop
+      >
+        <source
+          src="https://storage.googleapis.com/geometric-rex-343406.appspot.com/Video/Sandora%20Wallet.mp4"
+          type="video/mp4"
+        ></source>
+      </video>
     </div>
   );
 };
